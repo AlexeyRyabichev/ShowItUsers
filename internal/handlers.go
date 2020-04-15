7030091db5b8940d3ca6efda1e0b8b5315ada493
Alexey Ryabichev <alexey.ryabichev@gmail.com>
2020-04-16 02:53:22 +0300

    -

--- internal/handlers.go
+++ internal/handlers.go
@@ -1,12 +1,32 @@
 package internal
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func (rt *Router) PostUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	key := r.Header.Get("X-Token")
+
+	if key != rt.cfg.Token {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	var req map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for key, value := range req {
+		log.Printf("%s: %s", key, value)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	//fmt.Fprintf(w, "Hello World!")
 }
 
 func (rt *Router) GetUser(w http.ResponseWriter, r *http.Request) {
